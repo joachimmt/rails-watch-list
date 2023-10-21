4386e3b23aae17d6173377017936a26105af676d
joachim miot <joachimmt55@gmail.com>
2023-10-21 12:25:07 -0400

    Validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  validates_length_of :comment, :minimum => 6
+  validates :movie_id, uniqueness: { scope: :list_id, message: "This movie is already in the list." }
+  belongs_to :movie
+  belongs_to :list
 end
