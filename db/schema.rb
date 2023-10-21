5641c280b5f9626377aa3fc554bab728e5b2b5b0
joachim miot <joachimmt55@gmail.com>
2023-10-21 15:13:46 -0400

    models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_160431) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_190516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_160431) do
     t.string "poster_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 
 end
