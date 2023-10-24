c3dadeeb8b2d79c12fd7328a92951c7d934550a4
joachim miot <joachimmt55@gmail.com>
2023-10-24 19:52:08 -0400

    watch list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_190516) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_233602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,4 +37,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_190516) do
     t.integer "rating"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_reviews_on_list_id"
+  end
+
+  add_foreign_key "reviews", "lists"
 end
