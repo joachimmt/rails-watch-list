3427c02bd1e58ac68d0c5d78872af825a25cc1c1
joachim miot <joachimmt55@gmail.com>
2023-10-24 21:13:12 -0400

    mouliner

--- db/migrate/20231025010512_create_movies.rb
+++ db/migrate/20231025010512_create_movies.rb
@@ -2,8 +2,9 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :overview
+      t.text :overview
       t.string :poster_url
+      t.float :rating
 
       t.timestamps
     end
