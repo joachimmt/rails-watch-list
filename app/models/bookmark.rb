72154600514b734f1fad6de8d665b9fc430c0e1f
joachim miot <joachimmt55@gmail.com>
2023-10-26 12:59:31 -0400

    mouline

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
-  validates_length_of :comment, :minimum => 6
-  validates :movie_id, uniqueness: { scope: :list_id, message: "This movie is already in the list." }
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
